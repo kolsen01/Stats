a5126820428c2bfeb82e89d75868c6e6c248a138
Kensey Elliot Olsen <kenseyolsen1@Kenseys-MacBook-Pro.local>
2017-10-10 13:41:36 -0400

    Updating equations for CLT and random variables

--- CLT_random_number_generator/random_num_CLT.py
+++ CLT_random_number_generator/random_num_CLT.py
@@ -4,24 +4,29 @@ import scipy, math
 import matplotlib.pyplot as plt
 import pylab 
 
-random_list = []
+random_list_i = []
+random_list_j = []
 random_list_sq = []
+
 def Chi(N):
 	for y in range(0,N):
 		x_i = random.random()
-		random_list.append(x_i)
-		chi = sum(random_list)/N
+		x_j = random.random()
+		random_list_i.append(x_i)
+		random_list_j.append(x_j)
+		chi = (sum(random_list_i) + sum(random_list_j))/N 
 
-		z = (x_i)**2
-		random_list_sq.append(z)
-		chi_sq = sum(random_list_sq)/N
-		return chi, chi_sq
+		#z = (x_i)**2
+		#random_list_sq.append(z)
+		#chi_sq = sum(random_list_sq)/N
+		#return chi, chi_sq
+		return chi
 
-N2a, N2b = Chi(2)
-N3a, N3b = Chi(3)
-N4a, N4b = Chi(4)
-N10a, N10b = Chi(10)
-print (N2a,N2b,N3a,N3b,N4a,N4b,N10a,N10b)
+#N2a, N2b = Chi(2)
+#N3a, N3b = Chi(3)
+#N4a, N4b = Chi(4)
+#N10a, N10b = Chi(10)
+#print (N2a,N2b,N3a,N3b,N4a,N4b,N10a,N10b)
 
 x_exp = N2a +N3a +N4a + N10a
 x_exp_sq = N2b + N3b + N4b + N10b 
