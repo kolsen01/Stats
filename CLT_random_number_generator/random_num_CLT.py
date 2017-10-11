957a88b0720f4cb9eaf62e1ce142e8bfd7d2a5b9
Kensey Elliot Olsen <kenseyolsen1@Kenseys-MacBook-Pro.local>
2017-10-10 20:06:07 -0400

    Redefining chi equation

--- CLT_random_number_generator/random_num_CLT.py
+++ CLT_random_number_generator/random_num_CLT.py
@@ -6,10 +6,10 @@ import pylab
 
 random_list_i = []
 random_list_j = []
-random_list_sq = []
+#random_list_sq = []
 
 def Chi(N):
-	for y in range(0,N):
+	for i in range(0,N):
 		x_i = random.random()
 		x_j = random.random()
 		random_list_i.append(x_i)
@@ -22,18 +22,19 @@ def Chi(N):
 		#return chi, chi_sq
 		return chi
 
-#N2a, N2b = Chi(2)
-#N3a, N3b = Chi(3)
-#N4a, N4b = Chi(4)
-#N10a, N10b = Chi(10)
-#print (N2a,N2b,N3a,N3b,N4a,N4b,N10a,N10b)
+N2a, N2b = Chi(3)
+N3a, N3b = Chi(4)
+N4a, N4b = Chi(6)
+N10a, N10b = Chi(11)
 
-x_exp = N2a +N3a +N4a + N10a
-x_exp_sq = N2b + N3b + N4b + N10b 
+print (N2a,N2b,N3a,N3b,N4a,N4b,N10a,N10b)
+
+#x_exp = N2a +N3a +N4a + N10a
+#x_exp_sq = N2b + N3b + N4b + N10b 
 
 var_x =x_exp+ x_exp_sq
 print (var_x)
 
 plt.plot([2,3,4,10], [N2b,N3b,N4b,N10b], marker = 'o')
-plt.plot(var_x)
+#plt.plot(var_x)
 plt.show()
