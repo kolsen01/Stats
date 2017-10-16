3b19d096be41d597c0def41025698e842fe0b198
Kensey Elliot Olsen <kenseyolsen1@Kenseys-MacBook-Pro.local>
2017-10-16 19:41:01 -0400

    Added in functions and plots for poisson and gassian

--- distribution_curves/pmf_distributions.py
+++ distribution_curves/pmf_distributions.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import pylab 
 
+#for n=100,1000 and p=.5
+
 #binomial distribution
 def f(k,n,p):
 	y = (math.factorial(n))/(math.factorial(k)*math.factorial(n-k))
@@ -31,10 +33,52 @@ def po(k,n,p):
 
 p_vals1 = []
 for i in range(0,6): 
-	poisson = po(i,5,0.5)
+	poisson = po(i,100,0.5)
 	p_vals1.append(poisson)
 
 p_vals2 = []	
 for i in range(0,11): 
-	poisson = po(i,10,0.5)
-	p_vals2.append(poisson)		
+	poisson = po(i,1000,0.5)
+	p_vals2.append(poisson)	
+
+#gaussain distribution
+def ga(k,n,p):
+	y = 1/(math.sqrt(2*math.pi*n*p))
+	w = (-(k-n*p)**2)/(2*n*p)
+	x = (math.e)**w
+	ga = y*x
+	return ga	
+
+g_vals1 = []
+for i in range(0,6): 
+	gaussian = ga(i,100,0.5)
+	g_vals1.append(gaussian)
+
+g_vals2 = []	
+for i in range(0,11): 
+	gaussian = ga(i,1000,0.5)
+	g_vals2.append(gaussian)
+
+#Plots for binomial 1
+plt.plot(b_vals1, marker='o', color='r', label = 'Binomial')
+plt.plot(b_vals2, marker='o', color='b')
+
+#Plots for poisson1
+plt.plot(p_vals1, linestyle='--', marker='^', color='r', label = 'Poisson')
+plt.plot(p_vals2, linestyle='--', marker='^', color='b')
+
+#Plots for gaussian1
+plt.plot(g_vals1, linestyle='--', marker='s', color='r', label = 'Gaussian')
+plt.plot(g_vals2, linestyle='--', marker='s', color='b')
+
+#Details
+plt.ylabel('f(k,n,p)')
+plt.xlabel('k')
+
+pylab.legend(loc='upper right')
+plt.title('Binomial v. Poisson v. Gaussian')
+plt.axis([0,70,0,.33])
+
+plt.show()
+
+
